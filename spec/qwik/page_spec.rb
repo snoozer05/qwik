41e36b2e45e57fd51100bb26ab2c84cdd24c4b8b
SHIMADA Koji <snoozer.05@gmail.com>
2011-08-12 20:40:26 +0900

    add spec for page.rb

---
+++ spec/qwik/page_spec.rb
@@ -0,0 +1,10 @@
+require File.join(File.dirname(__FILE__), "..", "spec_helper")
+require 'qwik/group'
+
+module Qwik
+  describe Page do
+    subject { @site.get_pages.create_new }
+    its(:url) { should == '1.html' }
+    its(:key) { should == '1' }
+  end
+end
